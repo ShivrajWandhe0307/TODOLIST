d9f3620df3fc822662031d5fac36067061dd5148
ShivrajWandhe0309 <168719288+ShivrajWandhe0309@users.noreply.github.com>
2024-09-25 15:04:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [todoList, setTodoList] = useState([])
+
+
+  let saveToDoList = (event) => {
+    let toname = event.target.toname.value
+    if (!todoList.includes(toname)) {
+      let finalTodoList = [...todoList, toname]
+      setTodoList(finalTodoList)
+
+    }
+    else {
+      alert("ToDo Name Already Exists")
+    }
+
+    event.preventDefault();
+  }
+
+  let list=todoList.map((value,index)=>{
+    return(
+      <TodoListItem value={value} key={index} indexNumber={index} todoList={todoList} setTodoList={setTodoList}/>
+    )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO LIST</h1>
+      <form onSubmit={saveToDoList}>
+        <input type='text' name='toname' />
+        <button>Save</button>
+      </form>
+      <div className='outerDiv'>
+        <ul>
+          {list}
+        </ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function TodoListItem({value,indexNumber,todoList,setTodoList})
+{ let [status,setStatus]=useState(false)
+  let deleteRow=()=>
+  {
+    let finlData=todoList.filter((v,i)=>i!==indexNumber)
+    setTodoList(finlData)
+  }
+
+  let checkStatus=()=>{
+    setStatus(!status)
+  }
+  return(
+    <li className={status?'comple':''} onClick={checkStatus}>{indexNumber+1}   .{value} <span onClick={deleteRow}>&times;</span></li>
+  )
+}
